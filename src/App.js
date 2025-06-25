eb1a2fd3a643ccdc85baf2ca7af37cf76c99d3f3
unknown <somegetiye@gmail.com>
2025-06-25 01:45:27 -0700

    Complete all Four components: Logo, Form, PackageList, and the Statistic section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PakageList />
+      <Stat />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1 className="logo">🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return <h3 className="add-form">What you need for your 😍 Trip</h3>;
+}
+
+function PakageList() {
+  return <div className="list">List</div>;
+}
+function Stat() {
+  return (
+    <footer className="stats">
+      <em>
+        You have X item in your list, and you are already packed X item(X%)
+      </em>
+    </footer>
+  );
+}
